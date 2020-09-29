af3c7731c8ff494415bfc33c90bb144f561391c1
johnMorone <jmorone@alumni.princeton.edu>
2020-09-29 01:54:04 -0700

    I went crazy and implemented a fully custom autocomplete. Goodnight

--- src/DataList2.tsx
+++ src/DataList2.tsx
@@ -1,11 +1,18 @@
 //Based on: https://medium.com/@leofabrikant/react-autocomplete-with-react-virtualized-to-handle-massive-search-results-7865a8786972
 import * as React from "react";
-import {List, ListRowRenderer} from "react-virtualized";
-import {memo} from "react";
+import {areEqual, FixedSizeList as List} from "react-window";
+import "./DataList.scss";
+import {memo, useRef} from "react";
 
 export interface DataListProps {
   values: string[];
   id: string;
+  onSelect?: (
+    selected: string
+  ) => void;
+  onChange?: (
+    selected: string
+  ) => void;
 }
 
 export const cleanTerm = (term: string) =>
@@ -14,23 +21,103 @@ export const cleanTerm = (term: string) =>
 
 
 export const DataList2 = memo((props: DataListProps) => {
-  const {values, id} = props;
+  const {values, id, onChange, onSelect} = props;
 
-  const rowRenderer: ListRowRenderer = ({ key, index, style}) => {
-    const item = values[index];
-    return <option key={key} style={style} value={item}/>
-  };
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [lastHoverIndex, setLastHoverIndex] = React.useState(0);
+  const [showMenu, setShowMenu] = React.useState(false);
+  const [mouseSelect, setMouseSelect] = React.useState(false);
+
+  const listRef = useRef<List>(null);
+  const containerRef = useRef<HTMLElement | null>(null);
+
+  React.useEffect(() => {
+    if (containerRef) {
+      containerRef?.current?.setAttribute('tabIndex', '0');
+    }
+  }, [containerRef.current]);
+
+  // @ts-ignore
+  const Row = memo(({ data, index, style }) => {
+    // Data passed to List as "itemData" is available as props.data
+    const item = data[index];
+
+    return (
+      <div onMouseOver={() => {
+        if(lastHoverIndex !== index && mouseSelect) {
+          setActiveIndex(index);
+          setLastHoverIndex(index);
+        }
+      }}
+           onMouseMove={() => setMouseSelect(true)}
+           style={{...style, backgroundColor: index === activeIndex ?  "#0b195e" : "#111111"}}
+            onClick={() => {
+              setShowMenu(false) ;
+              onSelect && onSelect(values[index]);}}>
+        {item}
+      </div>
+    );
+    // @ts-ignore
+  }, areEqual)
+
 
   return (
-    <datalist id={id}>
-      <List
-        width={300}
-        height={999}
-        rowCount={values.length}
-        rowHeight={20}
-        rowRenderer={rowRenderer}
-      />,
-    </datalist>
+    <div className="data-list">
+      <span className="data-list-input">
+        <input
+          type="text"
+          id={id}
+          list="subreddits"
+          onChange={(e) => {onChange && onChange(e.target.value)}}
+          onClick={() => setShowMenu(true)}
+          onKeyDown={(e) =>  {
+            if (e.keyCode === 13) {
+              setShowMenu(false) ;
+              onSelect && onSelect(values[activeIndex]);
+            }
+            else if (e.keyCode === 38) {
+              if (activeIndex === 0) {
+                return;
+              }
+              setMouseSelect(false);
+              const newActiveIndex = activeIndex - 1;
+              setActiveIndex(newActiveIndex);
+              listRef.current?.scrollToItem(newActiveIndex)
+            } else if (e.keyCode === 40) {
+              if(!showMenu) {
+                setShowMenu(true);
+                return;
+              }
+              if (activeIndex === values.length - 1) {
+                return;
+              }
+              setMouseSelect(false);
+              const newActiveIndex = activeIndex + 1;
+              setActiveIndex(newActiveIndex);
+              listRef.current?.scrollToItem(newActiveIndex)
+            }
+          }}
+          onBlur={(e) => {
+            if(e.relatedTarget !== containerRef.current)
+            setShowMenu(false)
+          }}
+        />
+    </span>
+      {showMenu &&
+        <List
+            ref={listRef}
+            outerRef={containerRef}
+            width={"100%"}
+            height={220}
+            key={id}
+            itemCount={values.length}
+            itemData={values}
+            itemSize={40}
+        >
+          {Row}
+        </List>
+      }
+     </div>
   );
 })
 
